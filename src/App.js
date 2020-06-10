0be718a045a953db1f8cfd793984365512962c0c
github username <github email address>
2020-06-10 14:59:47 -0500

    redux is wild

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
-import reducer from './reducers'
-import { createStore } from 'redux';
+import ButtonGroup from "./components/ButtonGroup"
+import { store } from './store'
 
-const initialState = { tech: 'React'}
-const store = createStore(reducer, initialState);
 
 class App extends Component {
   render() {
-    return <HelloWorld tech={store.getState().tech} />;
+    return [
+      <HelloWorld key={1} tech={store.getState().tech} />,
+      <ButtonGroup key={2} technologies={["React", "Elm", "React-redux"]} />
+    ];
   }
 }
 
